b0f0ba16239b68eb1676e9ba997d5e4a3d157764
vinitpulstya <vinitsharma9876@gmail.com>
2023-07-02 16:10:42 +0530

    templated "projects" component

--- src/app/components/widgets/projects/projects.component.ts
+++ src/app/components/widgets/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SvgSafehtmlService } from 'src/app/services/svg-safehtml.service';
 
 @Component({
   selector: 'wgt-projects',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent {
+  constructor (private _svgDict: SvgSafehtmlService) {}
 
+  public github = this._svgDict.github;
+  public external = this._svgDict.external;
 }
--- src/app/services/svg-safehtml.service.ts
+++ src/app/services/svg-safehtml.service.ts
@@ -25,5 +25,9 @@ export class SvgSafehtmlService {
     '<svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-codepen"><title>CodePen</title><polygon points="12 2 22 8.5 22 15.5 12 22 2 15.5 2 8.5 12 2"></polygon><line x1="12" y1="22" x2="12" y2="15.5"></line><polyline points="22 8.5 12 15.5 2 8.5"></polyline><polyline points="2 15.5 12 8.5 22 15.5"></polyline><line x1="12" y1="2" x2="12" y2="8.5"></line></svg>'
   );
 
+  public readonly external: SafeHtml = this.sanitizer.bypassSecurityTrustHtml(
+    '<svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link"><title>External Link</title><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>'
+  );
+
   constructor(private sanitizer: DomSanitizer) { }
 }
