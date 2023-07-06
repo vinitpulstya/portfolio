d6baf0214a4a1f2dddac48a2cf9951bd612f9f2a
vinitpulstya <vinitsharma9876@gmail.com>
2023-07-07 02:41:34 +0530

    responsive header

--- src/app/components/widgets/header/header.component.ts
+++ src/app/components/widgets/header/header.component.ts
@@ -3,8 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  public readonly navlinks: Array<NavLinks> = [
+    { href: '/#about', display_text: 'About', style: 'animation-delay: 0ms' },
+    {
+      href: '/#jobs',
+      display_text: 'Experience',
+      style: 'animation-delay: 100ms',
+    },
+    {
+      href: '/#projects',
+      display_text: 'Work',
+      style: 'animation-delay: 200ms',
+    },
+    {
+      href: '/#contact',
+      display_text: 'Contact',
+      style: 'animation-delay: 300ms',
+    },
+  ];
 
+  public showMenu: Boolean = false;
+
+  public handleMenuClick = ():void => {
+    this.showMenu = !this.showMenu;
+  };
+}
+
+interface NavLinks {
+  href: string;
+  display_text: string;
+  style: string;
 }
